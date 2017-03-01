d15d979d56782b709e304c6377ed2261e0f8585a
Bor-Yuh Evan Chang <bec@cs.colorado.edu>
2017-03-01 09:43:11 -0700

    Add parser combinators dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
+  lazy val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.5"
 }
