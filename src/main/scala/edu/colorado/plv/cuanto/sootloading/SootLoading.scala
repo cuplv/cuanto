230cbf52becdad4720353283d2998ab63d232f32
Tianhan Lu <tianhan.lu@colorado.edu>
2017-03-19 13:42:59 -0600

    fix soot class path setup (now it works not only for *nix, but also for Windows)

--- src/main/scala/edu/colorado/plv/cuanto/sootloading/SootLoading.scala
+++ src/main/scala/edu/colorado/plv/cuanto/sootloading/SootLoading.scala
@@ -9,27 +9,31 @@ import scala.collection.JavaConverters._
   * Created by s on 3/17/17.
   */
 object SootLoading {
-  def getAnalysisResult[T](paths: List[String], main: Option[String] = None,analysis: Scene=>T): Option[T] ={
-    Options.v().keep_line_number()
-    Options.v().set_src_prec(Options.src_prec_class)
-    Options.v().set_process_dir(paths.asJava)
-    Options.v().unfriendly_mode() //allow to run with no command line args
-    Options.v().set_allow_phantom_refs(true)
-    Options.v().set_whole_program(true)
-    main match{
-      case Some(main) => Options.v().set_main_class(main)
-      case None =>
+    val fileSep: String = System.getProperty("file.separator")
+    val pathSep: String = System.getProperty("path.separator")
+
+    def getAnalysisResult[T](paths: List[String], main: Option[String] = None, analysis: Scene => T): Option[T] = {
+        Options.v().keep_line_number()
+        Options.v().set_src_prec(Options.src_prec_class)
+        Options.v().set_process_dir(paths.asJava)
+        Options.v().unfriendly_mode() //allow to run with no command line args
+        Options.v().set_allow_phantom_refs(true)
+        Options.v().set_whole_program(true)
+        main match {
+            case Some(_main) => Options.v().set_main_class(_main)
+            case None =>
+        }
+
+        val javaLibraryPath = System.getProperty("java.home") + fileSep + "lib" + fileSep
+        val jcePath = javaLibraryPath + "jce.jar"
+        val rtPath = javaLibraryPath + "rt.jar"
+        
+        println(pathSep)
+        Scene.v().setSootClassPath("." + pathSep + jcePath + pathSep + rtPath)
+        val getJimple: GetJimple[T] = new GetJimple(analysis)
+        PackManager.v().getPack("wjtp").add(new Transform("wjtp.get_jimple", getJimple))
+        soot.Main.main(Array("-unfriendly-mode"))
+        getJimple.result
     }
-
-    val path: String = Scene.v().getSootClassPath
-
-    val s: String = sys.env("JAVA_HOME") + "/jre/lib/rt.jar"
-    println(s)
-    Scene.v().setSootClassPath(path + ":" + s)
-    val getJimple: GetJimple[T] = new GetJimple(analysis)
-    PackManager.v().getPack("wjtp").add(new Transform("wjtp.get_jimple", getJimple))
-    soot.Main.main(Array("-unfriendly-mode"))
-    getJimple.result
-  }
 
 }
