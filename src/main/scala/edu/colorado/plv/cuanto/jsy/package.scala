3dbc8fc0318b593197d75f4dada766376a7791dd
Benno Stein <benno.stein@gmail.com>
2017-03-21 11:51:36 -0600

    Add abstract syntax data types.

--- src/main/scala/edu/colorado/plv/cuanto/jsy/package.scala
+++ src/main/scala/edu/colorado/plv/cuanto/jsy/package.scala
@@ -22,4 +22,27 @@ package jsy {
   /** Binary operators ''bop''. */
   trait Bop
 
+  /** Values ''v'' with ''e'' ::= ''v''. */
+  trait Val extends Expr
+
+  /** Field access ''e'' ::= ''e,,1,,''[''e,,2,,'']. */
+  case class Field(e1: Expr, e2: Expr) extends Expr
+
+  /** Variables ''x'' with ''e'' ::= ''x''. */
+  case class Var(name: String) extends Expr
+
+  /** Assignments ''e'' ::= ''e,,l,,'' := ''e,,r,,''.*/
+  case class Assign(l: LVal, r: Expr) extends Expr
+
+  /** Expression sequencing ''e'' ::= ''e,,1,,'' ; ''e,,2,,''. */
+  case class Seq(e1: Expr, e2: Expr) extends Expr
+
+  /** Function abstractions ''e'' ::= fix ''f'' ''xs'' . ''e''. */
+  case class Fix(f: Var, xs: Seq[Var], e: Expr) extends Expr
+
+  /** Function applications ''e'' ::= ''f'' ''xs''. */
+  case class App(f: Expr, xs: Seq[Expr]) extends Expr
+
+  /** Variable bindings ''e'' ::= let ''x'' = ''e,,1,,'' in ''e,,2,,''. */
+  case class Let(x: Var, e1: Expr, e2: Expr) extends Expr
 }
