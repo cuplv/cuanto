cbea3f1ee3cf00bf4f1a346fde3b25a46653ac15
Shawn <shawn.meier@colorado.edu>
2017-03-23 14:31:32 -0600

    concurrent rerun fix

--- src/main/scala/edu/colorado/plv/cuanto/sootloading/SootLoading.scala
+++ src/main/scala/edu/colorado/plv/cuanto/sootloading/SootLoading.scala
@@ -1,5 +1,7 @@
 package edu.colorado.plv.cuanto.sootloading
 
+import java.util.concurrent.locks.ReentrantLock
+
 import soot.options.Options
 import soot._
 
@@ -9,7 +11,12 @@ import scala.collection.JavaConverters._
   * Created by s on 3/17/17.
   */
 object SootLoading {
+  //Note: soot uses a lot of global static variables, we can only run one copy at a time and it must be reset
+  val lock : ReentrantLock = new ReentrantLock()
+
+
   def getAnalysisResult[T](paths: List[String], main: Option[String] = None,analysis: Scene=>T): Option[T] ={
+    lock.lock()
     Options.v().keep_line_number()
     Options.v().set_src_prec(Options.src_prec_class)
     Options.v().set_process_dir(paths.asJava)
@@ -22,14 +29,17 @@ object SootLoading {
     }
 
     val path: String = Scene.v().getSootClassPath
-
-    val s: String = sys.env("JAVA_HOME") + "/jre/lib/rt.jar"
+    val sep: String = System.getProperty("file.separator")
+    val s: String =  System.getProperty("java.home") +  sep + "jre" + sep + "lib" + sep + "rt.jar"
     println(s)
     Scene.v().setSootClassPath(path + ":" + s)
     val getJimple: GetJimple[T] = new GetJimple(analysis)
     PackManager.v().getPack("wjtp").add(new Transform("wjtp.get_jimple", getJimple))
     soot.Main.main(Array("-unfriendly-mode"))
+    lock.unlock()
+    G.reset()
     getJimple.result
+
   }
 
 }
