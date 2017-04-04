0b5ec8a8314825578a00a694e042ae88b9b84762
Bor-Yuh Evan Chang <bec@cs.colorado.edu>
2017-04-04 13:45:42 -0600

    Implement binding
    Introduce statement language in the concrete syntax for declarations.

--- src/main/scala/edu/colorado/plv/cuanto/jsy/binding/package.scala
+++ src/main/scala/edu/colorado/plv/cuanto/jsy/binding/package.scala
@@ -1,18 +1,42 @@
 package edu.colorado.plv.cuanto.jsy
 
-/** Define an string sub-language.
-  *
-  * @author Kyle Headley
-  */
 package binding {
 
-  /* Literals and values. */
+  /** @group Abstract Syntax Nodes */
+  case object Unit extends Val
 
-  /** Variables ''e'' ::= ''s''. */
-  case class Var(s: String) extends Expr
+  /** A Var node in an expression is a variable use.
+    *
+    * The name of the variable is stored privately.
+    *
+    * @group Abstract Syntax Nodes
+    */
+  case class Var(private val x: String) extends Expr
 
-  /** Variable binding: ''e'' ::= let ''v'' = ''e,,1,,'' in ''e,,2,,''. */
-  case class Bind(v: Expr, e1: Expr, e2: Expr) extends Expr
+  /** Let-binding.
+    *
+    * @param x the variable
+    * @param e1 the binding expression
+    * @param e2 the continuation expression
+    *
+    * @group Abstract Syntax Nodes
+    */
+  case class Bind(x: Var, e1: Expr, e2: Expr) extends Expr
+
+  case object Seq extends Bop
 
 }
 
+/** Define a sub-language with variables and binding.
+  *
+  * ==Abstract Syntax==
+  *
+  *   - ''v'' ::= `undefined` ≡ `Unit`
+  *   - ''e'' ::= ''x'' | `let` ''x'' `=` ''e,,1,,'' `.` ''e,,2,,'' ≡ `Bind(''x'',''e,,1,,'',e,,2,,)`
+  *             | ''e,,1,,'' , ''e,,2,,'' ≡ `Binary(Seq,''e,,1,,'',''e,,2'')`
+  *   - ''x'' ϵ `Var`
+  *
+  * @author Kyle Headley
+  * @author Bor-Yuh Evan Chang
+  */
+package object binding
--- src/test/scala/edu/colorado/plv/cuanto/jsy/binding/BindingParserSpec.scala
+++ src/test/scala/edu/colorado/plv/cuanto/jsy/binding/BindingParserSpec.scala
@@ -8,6 +8,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 /**
   * @author Kyle Headley
+  * @author Bor-Yuh Evan Chang
   */
 class BindingParserSpec extends FlatSpec with Matchers with PropertyChecks {
 
@@ -15,13 +16,57 @@ class BindingParserSpec extends FlatSpec with Matchers with PropertyChecks {
 
   val positives = Table(
     "concrete" -> "abstract",
+    // expressions
+    "x" -> Var("x"),
     "two" -> Var("two"),
-    "Let three = two in six" -> Bind(Var("three"),Var("two"),Var("six"))
+    "abc123" -> Var("abc123"),
+    "let x = two; x"
+      -> Bind(Var("x"), Var("two"), Var("x")),
+    "undefined" -> Unit,
+    "undefined, undefined" -> Binary(Seq, Unit, Unit),
+    // blocks
+    "{ x }" -> Var("x"),
+    "{ let x = two; x }"
+      -> Bind(Var("x"), Var("two"), Var("x")),
+    // declarations
+    "let x = two"
+      -> Bind(Var("x"), Var("two"), Unit),
+    "let x = two; let y = three"
+      -> Bind(Var("x"), Var("two"),
+           Bind(Var("y"), Var("three"), Unit)
+         ),
+    // statements
+    "x; y" -> Binary(Seq, Var("x"), Var("y"))
+  )
+
+  val flexibles = Table(
+    "concrete" -> "abstract",
+    "{ let x = y y }"
+      -> Bind(Var("x"), Var("y"), Var("y"))
+  )
+
+  val negatives = Table(
+    "concrete",
+    "123abc",
+    "(let x = y)",
+    "(x; y)"
   )
 
   forAll (positives) { (conc, abs) =>
     it should s"parse $conc into $abs" in {
       parse(conc) shouldEqual abs
+    }
+  }
+
+  forAll (flexibles) { (conc, abs) =>
+    it should s"flexibly parse $conc into $abs" in {
+      parse(conc) shouldEqual abs
+    }
+  }
+
+  forAll (negatives) { conc =>
+    it should s"not parse $conc" in {
+      parse(conc) should be a 'failure
     }
   }
 
