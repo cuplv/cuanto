b1697882e226ae9e63a6e3c154c46fa0900cb4cf
Shawn <shawn.meier@colorado.edu>
2017-04-18 11:30:30 -0600

    soot loading separated from other changes

--- src/main/scala/edu/colorado/plv/cuanto/soot/sootloading/GetJimple.scala
+++ src/main/scala/edu/colorado/plv/cuanto/soot/sootloading/GetJimple.scala
@@ -1,18 +1,18 @@
-package edu.colorado.plv.cuanto.sootloading
+package edu.colorado.plv.cuanto.scoot.sootloading
 
 import java.util
 
 import soot.{Scene, SceneTransformer}
 import scala.collection.JavaConverters._
 
-
 /**
   * Created by s on 3/17/17.
   */
-class GetJimple[T](analysis: Scene=>T) extends SceneTransformer {
+class GetJimple[T](analysis: Scene => T) extends SceneTransformer {
   var result: Option[T] = None
 
   override def internalTransform(s: String, map: util.Map[String, String]): Unit = {
     result = Some(analysis(Scene.v()))
+    System.err.println(Scene.v().getApplicationClasses)
   }
 }
---
+++ src/test/resources/test_files/test1/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 1;
+		boolean c = true;
+		for (int i = 0; i < a && c; i++) {
+		    b = b * (a - 2) / 2 ;
+        }
+	}
+}
