0db1a083aa0eeb218b635bc59f9d5903ed279aa3
octalsrc <nicklewchenko92@gmail.com>
2017-04-19 14:43:36 -0600

    Fix unapply for BinopExpr (move the def to companion object)

--- src/main/scala/edu/colorado/plv/cuanto/scoot/ir/BinopExpr.scala
+++ src/main/scala/edu/colorado/plv/cuanto/scoot/ir/BinopExpr.scala
@@ -9,7 +9,10 @@ class BinopExpr(dt: soot.jimple.BinopExpr) extends Expr(dt: soot.jimple.Expr) {
   def op2: Value = new Value(dt.getOp2())
 
   def symbol: String = dt.getSymbol()
+}
 
+object BinopExpr {
+  // Unapply needs to go in a "companion object", it seems
   def unapply(e: BinopExpr): Option[(Value, Value)] =
     Some((e.op1, e.op2))
 }
