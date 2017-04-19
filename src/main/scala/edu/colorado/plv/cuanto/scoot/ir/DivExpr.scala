0cd6afe210264d9881637a9a86d05989ccbed058
Jared Wright <jawr6028@colorado.edu>
2017-04-19 14:50:46 -0600

    fix inheritance structure of AddExpr and DivExpr

--- src/main/scala/edu/colorado/plv/cuanto/scoot/ir/DivExpr.scala
+++ src/main/scala/edu/colorado/plv/cuanto/scoot/ir/DivExpr.scala
@@ -3,6 +3,6 @@ package edu.colorado.plv.cuanto.scoot.ir
 /**
   * Created by Jared on 4/17/2017.
   */
-class DivExpr(dt: soot.jimple.DivExpr) extends Expr(dt: soot.jimple.Expr)
+class DivExpr(dt: soot.jimple.DivExpr) extends BinopExpr(dt: soot.jimple.BinopExpr)
   //empty interface
 
