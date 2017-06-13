f0972dffb2c867f2e9dc82f1c84e6aee11ea7a22
Jared Wright <jawr6028@colorado.edu>
2017-06-13 11:15:43 -0600

    Add package object and Value skeleton

---
+++ src/main/scala/edu/colorado/plv/cuanto/scoot/ir/Value.scala
@@ -0,0 +1,8 @@
+package edu.colorado.plv.cuanto.scoot.ir
+
+/**
+  * Created by Jared on 6/13/2017.
+  */
+class Value {
+
+}
---
+++ src/main/scala/edu/colorado/plv/cuanto/scoot/ir/package.scala
@@ -0,0 +1,8 @@
+package edu.colorado.plv.cuanto.scoot
+
+/**
+  * Created by Jared on 6/13/2017.
+  */
+package object ir {
+
+}
