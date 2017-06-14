34b5fe8b015d16a948eb46e949e5c473348e84f1
Shawn Meier <shawn.meier@colorado.edu>
2017-06-14 10:55:17 -0600

    soot getir works

--- src/main/scala/edu/colorado/plv/cuanto/soot/sootloading/GetSootIR.scala
+++ src/main/scala/edu/colorado/plv/cuanto/soot/sootloading/GetSootIR.scala
@@ -21,6 +21,7 @@ object GetSootIR {
   def initSootTest(paths: List[String], main: Option[String]): Scene = {
 
     soot.G.reset()
+
     Options.v().keep_line_number() //Soot does not automatically retain line number info, this option keeps it
     Options.v().set_src_prec(Options.src_prec_class) //Type of files in source to retain
     Options.v().set_process_dir(paths.asJava) //set the list of java paths to consider
@@ -28,25 +29,22 @@ object GetSootIR {
     //TODO: figure out best way to deal with this
     Options.v().set_allow_phantom_refs(true) //Create fake versions of classes which cannot be resolved (DANGER!)
     Options.v().set_whole_program(true) //Option needed to run whole jimple program transformations
-    val scene = soot.Scene.v()
+    main foreach { main => Options.v().set_main_class(main) }
+    val scene = Scene.v()
+    //Set single entry point
+    val path: String = scene.getSootClassPath
+    //Combine paths into semi colon separated list
+    scene.setSootClassPath(path + ":" + paths.foldLeft("")((acc, str) =>
+      acc + str + pathSep) + jcePath + pathSep + rtPath)
+
     scene.loadBasicClasses()
     if(paths.length < 1){
       throw new IllegalArgumentException("At least one path needed")
     }
-    main foreach { main => Options.v().set_main_class(main) }  //Set single entry point
-    val path: String = Scene.v().getSootClassPath
-    //Combine paths into semi colon separated list
-    Scene.v().setSootClassPath(path + ":" + paths.foldLeft("")((acc, str) =>
-      acc + str + pathSep) + jcePath + pathSep + rtPath)
-
-//    PackManager.v().runPacks()
-//    ShimpleTransformer.v.transform()
     scene.loadDynamicClasses()
+    scene.loadNecessaryClasses()
     PackManager.v().getPack("wjpp").apply();
     PackManager.v().getPack("cg").apply();
-//    PackManager.v().getPack("wjtp").apply();
-//    PackManager.v().getPack("wjop").apply();
-//    PackManager.v().getPack("wjap").apply();
 
     scene
   }
--- src/test/scala/edu/colorado/plv/cuanto/soot/sootloading/GetSootIRSpec.scala
+++ src/test/scala/edu/colorado/plv/cuanto/soot/sootloading/GetSootIRSpec.scala
@@ -2,6 +2,7 @@ package edu.colorado.plv.cuanto.soot.sootloading
 
 import edu.colorado.plv.cuanto.soot.sootloading.GetSootIR.getClass
 import org.scalatest.{FlatSpec, Matchers}
+import soot.jimple.JimpleBody
 import soot.{Scene, SootClass}
 import soot.util.Chain
 
@@ -12,12 +13,24 @@ import scala.collection.JavaConverters._
   * Created by s on 6/9/17.
   */
 class GetSootIRSpec extends FlatSpec with Matchers {
-  "GetSootIR" should "get intermediate representation" in {
-    val scene: Scene = GetSootIR.initSootTest(List("/Users/s/Documents/source/cuanto/test_files/test1"),None)
+  "GetSootIR" should "load a set of non empty classes" in {
+    val scene: Scene = GetSootIR.initSootTest(List("/Users/s/Documents/source/cuanto/test_files/test1/bin/"),None)
 
     val classes = scene.getClasses().toArray
     assert(classes.length > 0)
 
+  }
+  "GetSootIR" should "load a specific class" in {
+    val scene: Scene = GetSootIR.initSootTest(List("/Users/s/Documents/source/cuanto/test_files/test1/bin/"),Some("Test1"))
+//    val classes = scene.getClasses().toArray()
+    scene.getSootClass("Test1").isPhantom() shouldBe false
+  }
+  "GetSootIR" should "load jimple bytecode" in{
+    val scene: Scene = GetSootIR.initSootTest(List("/Users/s/Documents/source/cuanto/test_files/test1/bin/"),Some("Test1"))
+    val methods = scene.getSootClass("Test1").getMethods()
+    val method = scene.getSootClass("Test1").getMethod("void main(java.lang.String[])")
+    methods.size() shouldBe 2
+    method.getActiveBody shouldBe a[JimpleBody]
   }
 
 }
