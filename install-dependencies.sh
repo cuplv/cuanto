1ba5c48fc3a723344c90209788042d5183a27814
Tianhan Lu <tianhan.lu@colorado.edu>
2017-07-08 16:59:18 -0600

    debugging java native library path on travis

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,6 +4,7 @@ find /usr/lib/jvm/java-8-oracle -name "jni.h"
 find /usr/lib/jvm/java-8-oracle -name "jni_md.h"
 
 echo $JAVA_HOME
+echo $pwd
 
 svn co svn://scm.gforge.inria.fr/svnroot/apron/apron/trunk apron
 python modify-makefile.py
@@ -11,7 +12,7 @@ cd apron
 make
 make install
 cd ..
-mv apron/japron/libjapron.so ./lib/
-mv apron/japron/libjgmp.so ./lib/
-mv apron/japron/apron.jar ./lib/
-mv apron/japron/gmp.jar ./lib/
+mv apron/japron/libjapron.so lib/
+mv apron/japron/libjgmp.so lib/
+mv apron/japron/apron.jar lib/
+mv apron/japron/gmp.jar lib/
