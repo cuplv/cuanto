ebbfaf5fd2bbba8d31a241e2899709b86f569f4b
octalsrc <nicklewchenko92@gmail.com>
2017-07-21 08:57:23 -0600

    Change travis config to use nix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-{ stdenv, sbt, z3, japron, jdk }:
+{ stdenv, sbt, z3, jdk }:
 
 stdenv.mkDerivation rec {
   name = "cuanto-${version}";
   version = "dev";
   src = ./.;
-  buildInputs = [ sbt z3 japron jdk ];
+  buildInputs = [ sbt z3 (jdk.override {minimal = true;}) ];
 }
