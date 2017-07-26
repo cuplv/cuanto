a915b1dcd8f80066be9c98782613db8464c9c137
Shawn Meier <shawn.meier@colorado.edu>
2017-07-26 14:41:28 -0600

    added unit test file

---
+++ src/test/scala/edu/colorado/plv/cuanto/soot/sootloading/InterpreterSpec.scala
@@ -0,0 +1,10 @@
+package edu.colorado.plv.cuanto.soot.sootloading
+
+import org.scalatest.FlatSpec
+
+/**
+  * Created by Shawn Meier on 7/26/17.
+  */
+class InterpreterSpec extends FlatSpec {
+  
+}
