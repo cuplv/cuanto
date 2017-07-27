e7ef55e571afa80731f196e42159b316c3a679d1
octalsrc <nicklewchenko92@gmail.com>
2017-07-27 11:11:24 -0600

    Use isolated $CUANTO_CLASSPATH for system-installed jars

--- project/SystemClasspath.scala
+++ project/SystemClasspath.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object SystemClasspath {
 
-  private[this] val systemClasspathRaw =
-    sys.env.get("CLASSPATH") match {
+  private[this] val systemClasspathRaw = (pathName: String) =>
+    sys.env.get(pathName) match {
       case Some(path) => path
       case _ => ""
     }
 
-  private[this] val jars: Seq[File] =
-    systemClasspathRaw.split(':').map(file).toSeq
+  private[this] val jars: String => Seq[File] = pathName => 
+    systemClasspathRaw(pathName).split(':').map(file).toSeq
 
-  val systemClasspath = jars.classpath
+  val systemClasspath = (pathName: String) => jars(pathName).classpath
 
 }
