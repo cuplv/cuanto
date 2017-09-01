74585f5fda22dd7c4ce666e7483013edfcda7815
Bor-Yuh Evan Chang <bec@cs.colorado.edu>
2017-09-01 11:28:00 -0600

    Define a way to define fixture objects and traverse them (#66)
    * Add traversable fixtures
    
    * Add checking of the Walatest fixture
    
    * Clean up Walaclient spec
    
    * Name the Walatest value in a more clear manner
    
    * Clean up style

--- src/test/scala/edu/colorado/plv/cuanto/wala/ClientSpec.scala
+++ src/test/scala/edu/colorado/plv/cuanto/wala/ClientSpec.scala
@@ -1,36 +1,29 @@
 package edu.colorado.plv.cuanto.wala
 
 import org.scalatest.FlatSpec
-import edu.colorado.plv.cuanto.jutil.implicits._
-import edu.colorado.plv.cuanto.walatest.EmptyMainTest
 
 /**
   * @author Bor-Yuh Evan Chang
   */
 class ClientSpec extends FlatSpec {
-  import edu.colorado.plv.cuanto.wala._
+  import edu.colorado.plv.cuanto.tests.Walatest
 
-  "apply" should "not crash" in {
-    val url = classOf[EmptyMainTest].getRelativeURL.get
-    Client(url)
-  }
-
-  lazy val walaTest = Client(classOf[EmptyMainTest].getRelativeURL.get)
+  lazy val emptyMainTest = Client(Walatest.emptyMainTestURL)
 
   "classHierarchy" should "not crash" in {
-    walaTest.classHierarchy
+    emptyMainTest.classHierarchy
   }
 
   "applicationClasses" should "get the right number of application classes" in {
-    assertResult(1) { walaTest.applicationClasses.size }
+    assertResult(1) { emptyMainTest.applicationClasses.size }
   }
 
   "makeIR" should "get the right number of methods" in {
-    assertResult(2) { walaTest.makeIR(walaTest.applicationClasses).size }
+    assertResult(2) { emptyMainTest.makeIR(emptyMainTest.applicationClasses).size }
   }
 
   it should "get the right number of main methods" in {
-    assertResult(1) { walaTest.makeIR(walaTest.applicationClasses, Client.MainSelector).size }
+    assertResult(1) { emptyMainTest.makeIR(emptyMainTest.applicationClasses, Client.MainSelector).size }
   }
 
 }
