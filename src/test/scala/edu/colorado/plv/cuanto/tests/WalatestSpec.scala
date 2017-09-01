428a409832d82856792a8456a6049437d01cca36
Bor-Yuh Evan Chang <bec@cs.colorado.edu>
2017-09-01 10:39:12 -0600

    Add checking of the Walatest fixture

---
+++ src/test/scala/edu/colorado/plv/cuanto/tests/WalatestSpec.scala
@@ -0,0 +1,22 @@
+package edu.colorado.plv.cuanto
+package tests
+
+import java.net.URL
+
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * @author Bor-Yuh Evan Chang
+  */
+class WalatestSpec extends CuantoSpec with LazyLogging {
+  import testing.implicits._
+
+  behavior of "Walatest"
+
+  it should "should refer to a valid URLs" in {
+    for ((f,v) <- Walatest.asTraversable[URL]) {
+      logger.info(s"${f} = ${v}")
+    }
+  }
+
+}
